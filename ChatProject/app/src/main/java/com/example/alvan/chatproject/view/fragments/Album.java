714a9ea22d930f8c6280bada22e805007c2bb5b1
Alvantesha Priliandi <palvantesha@gmail.com>
2016-12-21 17:54:16 +0700

    Card view

---
+++ ChatProject/app/src/main/java/com/example/alvan/chatproject/view/fragments/Album.java
@@ -0,0 +1,45 @@
+package com.example.alvan.chatproject.view.fragments;
+
+/**
+ * Created by alvan on 21/12/16.
+ */
+
+public class Album {
+    private String name;
+    private int numOfSongs;
+    private int thumbnail;
+
+    public Album() {
+    }
+
+    public Album(String name, int numOfSongs, int thumbnail) {
+        this.name = name;
+        this.numOfSongs = numOfSongs;
+        this.thumbnail = thumbnail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumOfSongs() {
+        return numOfSongs;
+    }
+
+    public void setNumOfSongs(int numOfSongs) {
+        this.numOfSongs = numOfSongs;
+    }
+
+    public int getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(int thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+}
